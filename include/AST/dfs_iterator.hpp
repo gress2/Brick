b104f1146ffd0f807cb949fb0f28edd3b5480675
Collin Gress <gress126@gmail.com>
2018-04-06 13:34:12 -0500

    update

--- include/AST/dfs_iterator.hpp
+++ include/AST/dfs_iterator.hpp
@@ -5,17 +5,62 @@ namespace brick::AST
 {
   template <class Tree>
   class dfs_iterator {
+    public:
+      // TODO: typedefs
+      using difference_type = std::ptrdiff_t;
+      using value_type = Tree;
     private:
       Tree* tree_;
     public:
+      dfs_iterator();
       dfs_iterator(Tree*);
+      value_type& operator*() const;
+      dfs_iterator operator++(int);
+      dfs_iterator& operator++();
+      dfs_iterator operator--(int);
+      dfs_iterator& operator--();
+      dfs_iterator operator+(difference_type);
   };
+
+  template <class Tree>
+  dfs_iterator<Tree>::dfs_iterator()
+    : tree_(nullptr)
+  {}
 
   template <class Tree>
   dfs_iterator<Tree>::dfs_iterator(Tree* tree)
     : tree_(tree)
   {}
 
+  template <class Tree>
+  Tree& dfs_iterator<Tree>::operator*() const {
+    return *tree_;  
+  }
+
+  template <class Tree>
+  dfs_iterator<Tree> dfs_iterator<Tree>::operator++(int) {
+    return *this; 
+  }
+
+  template <class Tree>
+  dfs_iterator<Tree>& dfs_iterator<Tree>::operator++() {
+    return *this;
+  }
+  
+  template <class Tree>
+  dfs_iterator<Tree> dfs_iterator<Tree>::operator--(int) {
+    return *this;
+  }
+
+  template <class Tree>
+  dfs_iterator<Tree>& dfs_iterator<Tree>::operator--() {
+    return *this;
+  }
+
+  template <class Tree>
+  dfs_iterator<Tree> dfs_iterator<Tree>::operator+(dfs_iterator<Tree>::difference_type diff) {
+    return *this;
+  } 
 }
 
 #endif
--- test/tree_tests.cc
+++ test/tree_tests.cc
@@ -1,10 +1,23 @@
+#include <string>
+
 #include "brick.hpp"
 #include "gtest/gtest.h"
+
+template <class T>
+using tree = brick::AST::tree<T>;
 
 class TreeFixture : public ::testing::Test {
   protected:
     virtual void SetUp() {}
+    tree<std::string> root_{nullptr, "root"}; 
 };
+
+TEST_F(TreeFixture, Test1) { 
+  auto it = root_.begin_dfs();
+  auto& tmp = *it;
+  std::cout << tmp.get_value() << std::endl;
+  ASSERT_EQ(0, 0);
+}
 
 int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
