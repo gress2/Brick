0ee3109af3670e8758176607dcc34db616a3f95d
Collin Gress <gress2@illinois.edu>
2018-05-27 14:02:02 -0500

    adding antlr4

---
+++ test/parser/parser.cc
@@ -0,0 +1,8 @@
+#include <antlr4-runtime.h>
+#include "brick.hpp"
+#include "gtest/gtest.h"
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
